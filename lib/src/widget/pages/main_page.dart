20c854e0706470f35d4d9b24560497c62f69e712
Timofei <timabelov08@gmail.com>
2025-04-30 19:59:16 +0300

    dart analyze

--- lib/src/widget/pages/main_page.dart
+++ lib/src/widget/pages/main_page.dart
@@ -7,7 +7,6 @@ import 'package:flutter_hw_lototinder/src/domain/state/likes_history_notifier.da
 import 'package:flutter_hw_lototinder/src/navigation/navigation_manager.dart';
 import 'package:flutter_hw_lototinder/src/domain/state/images_history_notifier.dart';
 import 'package:flutter_hw_lototinder/src/domain/state/images_loader.dart';
-import 'package:flutter_hw_lototinder/src/utils/logger.dart';
 import 'package:flutter_hw_lototinder/src/widget/components/bottom_navigation_holder.dart';
 import 'package:flutter_hw_lototinder/src/widget/components/logo_icon.dart';
 import 'package:get_it/get_it.dart';
